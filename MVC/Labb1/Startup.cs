e9f9e58b9dca600d2581fe257f62e7428d03edf4
johanthorner <johan@thorner.se>
2016-04-18 14:51:24 +0200

    en första kommit

---
+++ MVC/Labb1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Labb1.Startup))]
+namespace Labb1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);//KOmmentar
+        }
+    }
+}
