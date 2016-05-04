837fb6054bd8a54ab850572efe66b24bd91a0746
johanthorner <johan@thorner.se>
2016-05-04 15:21:14 +0200

    labb 2 upg 2 torsdag

--- MVC/Labb2_upg2/Controllers/GameController.cs
+++ MVC/Labb2_upg2/Controllers/GameController.cs
@@ -11,49 +11,33 @@ namespace Labb2_upg2.Controllers
     public class GameController : Controller
     {
         // GET: Game
-        
+
         public ActionResult Index()
         {
-            Tiles.TileCordinates = new List<Cordinate>()
-            {
-                new Cordinate() {ID = 1, sign = '1', DisableButton = false},
-                new Cordinate() {ID = 2, sign = '2', DisableButton = false},
-                new Cordinate() {ID = 3, sign = '3', DisableButton = false},
-                new Cordinate() {ID = 4, sign = '4', DisableButton = false},
-                new Cordinate() {ID = 5, sign = '5', DisableButton = false},
-                new Cordinate() {ID = 6, sign = '6', DisableButton = false},
-                new Cordinate() {ID = 7, sign = '7', DisableButton = false},
-                new Cordinate() {ID = 8, sign = '8', DisableButton = false},
-                new Cordinate() {ID = 9, sign = '9', DisableButton = false}
-            };
+            GameModel.ResetTile();
             return View();
         }
 
         [HttpPost]
         public ActionResult Index(string send)
         {
-
-            for (int i = 0; i < Tiles.TileCordinates.Count; i++)
+            try
             {
-                if (Tiles.TileCordinates[i].ID == int.Parse(send))
+                if (send == "Reset Game")
                 {
-                    Tiles.TileCordinates[i].sign = 'X';
+                    GameModel.ResetTile();
                 }
+                else
+                {
+                    ViewBag.Message = GameModel.RunGameLogic(send);
+                }
+                return View();
             }
-
-            ViewBag.Message = Tiles.TileCordinates[0];
-            ViewBag.Message = GameModel.CheckForWinner('X');
-
-            for (int i = 0; i < Tiles.TileCordinates.Count; i++)
+            catch (Exception e)
             {
-                if (Tiles.TileCordinates[i].ID == GameModel.ComputerPlay())
-                {
-                    Tiles.TileCordinates[i].sign = 'o';
-                }
+                ViewBag.Message = "Choose an empty space";
+                return View();
             }
-            ViewBag.Message = GameModel.CheckForWinner('o');
-
-            return View();
         }
     }
 }
--- MVC/Labb2_upg2/Models/GameModel.cs
+++ MVC/Labb2_upg2/Models/GameModel.cs
@@ -24,18 +24,28 @@ namespace Labb2_upg2.Models
             }
             return horizontal;
         }
-
         public static string CheckForWinner(char checkForWin)
         {
             if (CheckForDiagonalWin(checkForWin) || CheckForVerticalWin(checkForWin) || CheckForHorizontalWin(checkForWin))
             {
                 if (checkForWin == 'X')
+                {
+                    ResetTile();
                     return "You are the winner!";
+                }
 
                 if (checkForWin == 'O')
+                {
+                    ResetTile();
                     return "You lost, the computer won...";
+                }
             }
-            return string.Empty;
+            if (CheckForTie())
+            {
+                ResetTile();
+                return "It's a tie! No one wins!";
+            }
+            return String.Empty;
         }
         public static bool CheckForDiagonalWin(char checkForSign)
         {
@@ -50,7 +60,6 @@ namespace Labb2_upg2.Models
             return false;
 
         }
-
         public static bool CheckForVerticalWin(char checkForSign)
         {
             for (int i = 0; i < 3; i ++)
@@ -62,7 +71,6 @@ namespace Labb2_upg2.Models
             }
             return false;
         }
-
         public static bool CheckForHorizontalWin(char checkForSign)
         {
             for (int i = 0; i < 9; i+=3)
@@ -74,14 +82,76 @@ namespace Labb2_upg2.Models
             }
             return false;
         }
+        public static bool CheckForTie()
+        {
+            int counter = 0;
+            foreach (var cordinate in Tiles.TileCordinates)
+            {
+                if (cordinate.sign == 'O' || cordinate.sign == 'X')
+                    counter++;
+            }
 
+            if (counter == 9)
+                return true;
+            return false;
+        }
         public static int ComputerPlay()
         {
-            return 1;
-
-
+            if (Tiles.TileCordinates[4].sign == '5')
+                return 5;
+            return RandomFreeSquare();
         }
+        public static void ChangeCordiante(int id, char sign)
+        {
+            for (int i = 0; i < Tiles.TileCordinates.Count; i++)
+            {
+                if (Tiles.TileCordinates[i].ID == id)
+                {
+                    Tiles.TileCordinates[i].sign = sign;
+                }
+            }
+        }
+        public static string RunGameLogic(string send)
+        {
+            ChangeCordiante(int.Parse(send), 'X');
+            string message = CheckForWinner('X');
+            if (message.Length>0)
+                return message;
 
+            ChangeCordiante(ComputerPlay(),'O');
+            message = CheckForWinner('O');
+            if (message.Length > 0)
+                return message;
+
+            message = CheckForWinner('O');
+            if (message.Length > 0)
+            {
+                return message;
+            }
+            return message;
+        }
+        public static int RandomFreeSquare()
+        {
+            List<int> freeSquares = new List<int>();
+            foreach (var cordinate in Tiles.TileCordinates)
+            {
+                if (cordinate.sign != 'X' && cordinate.sign != 'O')
+                {
+                    freeSquares.Add(cordinate.ID);
+                }
+            }
+            Random randomNr = new Random();
+            int squareId = randomNr.Next(0, freeSquares.Count - 1);
+            return freeSquares[squareId];
+        }
+        public static void ResetTile()
+        {
+            Tiles.TileCordinates = new List<Cordinate>();
+            for (int i = 1; i <= 9; i++)
+            {
+                Tiles.TileCordinates.Add(new Cordinate() { ID = i, sign = char.Parse(i.ToString()), DisableButton = false });
+            }
+        }
     }
 
 }
