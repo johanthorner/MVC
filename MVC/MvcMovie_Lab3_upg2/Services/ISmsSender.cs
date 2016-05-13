7bce4675d1cacc920a5718805631e76388189639
johanthorner <johan@thorner.se>
2016-05-13 12:31:28 +0200

    labb3_upg2_start

---
+++ MVC/MvcMovie_Lab3_upg2/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie_Lab3_upg2.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
---
+++ MVC/MvcMovie_Lab3_upg2/ViewModels/Manage/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie_Lab3_upg2.ViewModels.Manage
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
