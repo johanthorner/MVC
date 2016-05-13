92c1aceda9b0ac226264e369c15db844a872a4ab
johanthorner <johan@thorner.se>
2016-05-13 15:16:35 +0200

    Adding a view klar

---
+++ MVC/MvcMovie_Lab3_upg2/Controllers/HElloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.WebEncoders;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie_Lab3_upg2.Controllers
+{
+    public class HElloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // GET: /HelloWorld/Welcome/
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NUmTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
